ad35f702a27a37f9976f8463e31d319d13a4aaac
Adilet <masayoshizxc@gmail.com>
2022-09-05 11:53:47 +0600

    before nvram

---
+++ ImageTracker/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  ImageTracker
+//
+//  Created by Adilet on 3/9/22.
+//
+
+import UIKit
+import SnapKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
---
+++ Pods/Target Support Files/Pods-ImageTracker/Pods-ImageTracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImageTracker : NSObject
+@end
+@implementation PodsDummy_Pods_ImageTracker
+@end
