5e69e5eb0491c0968b7354a30c0838281bba7300
Adilet <masayoshizxc@gmail.com>
2022-09-07 00:34:43 +0600

    before player

--- ImageTracker/HomeViewController.swift
+++ ImageTracker/HomeViewController.swift
@@ -11,25 +11,25 @@ import AVFoundation
 
 class HomeViewController: UIViewController {
     var counter = 0
-    private lazy var flash : UIButton = {
-        let btn = UIButton()
-        btn.setImage(UIImage(systemName: "flashlight.on.fill"), for: .normal)
-        btn.tintColor = .systemOrange
-        btn.layer.borderWidth = 3
-        btn.layer.borderColor = UIColor.systemOrange.cgColor
-        btn.addTarget(self, action: #selector(tappedFlash), for: .touchUpInside)
-        btn.layer.cornerRadius = 30
-        return btn
-    }()
-//    private lazy var trackButton : UIButton = {
+//    private lazy var flash : UIButton = {
 //        let btn = UIButton()
-//        btn.backgroundColor = .systemOrange
-//        btn.setTitle("Start Tracking", for: .normal)
-//        btn.titleLabel?.font = .boldSystemFont(ofSize: 25)
-//        btn.addTarget(self, action: #selector(tappedTrack), for: .touchUpInside)
+//        btn.setImage(UIImage(systemName: "flashlight.on.fill"), for: .normal)
+//        btn.tintColor = .systemOrange
+//        btn.layer.borderWidth = 3
+//        btn.layer.borderColor = UIColor.systemOrange.cgColor
+//        btn.addTarget(self, action: #selector(tappedFlash), for: .touchUpInside)
+//        btn.layer.cornerRadius = 30
 //        return btn
-//
 //    }()
+    private lazy var trackButton : UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .systemOrange
+        btn.setTitle("Start Tracking", for: .normal)
+        btn.titleLabel?.font = .boldSystemFont(ofSize: 25)
+        btn.addTarget(self, action: #selector(tappedTrack), for: .touchUpInside)
+        return btn
+        
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubviews()
@@ -37,50 +37,50 @@ class HomeViewController: UIViewController {
     }
     
     func addSubviews(){
-        view.addSubview(flash)
-//        view.addSubview(trackButton)
+//        view.addSubview(flash)
+        view.addSubview(trackButton)
     }
     
     
     @objc func tappedTrack(){
         let vc = ViewController()
-//        vc.modalPresentationStyle = .fullScreen
+        vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true, completion: nil)
     }
     
-    @objc func tappedFlash(){
-        if (counter % 2 == 0){
-            toggleTorch(on: true)
-        }
-        else{
-            toggleTorch(on: false)
-        }
-        counter += 1
-    }
-    
-    func toggleTorch(on: Bool) {
-        guard
-            let device = AVCaptureDevice.default(for: AVMediaType.video),
-            device.hasTorch
-        else { return }
-
-        do {
-            try device.lockForConfiguration()
-            device.torchMode = on ? .on : .off
-            device.unlockForConfiguration()
-        } catch {
-            print("Torch could not be used")
-        }
-    }
-    func setUpConstraints(){
-        flash.snp.makeConstraints{make in
-            make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().inset(100)
-            make.width.height.equalTo(60)
-        }
-//        trackButton.snp.makeConstraints{make in
-//            make.centerX.centerY.equalToSuperview()
-//            make.left.right.equalToSuperview().inset(40)
+//    @objc func tappedFlash(){
+//        if (counter % 2 == 0){
+//            toggleTorch(on: true)
 //        }
+//        else{
+//            toggleTorch(on: false)
+//        }
+//        counter += 1
+//    }
+    
+//    func toggleTorch(on: Bool) {
+//        guard
+//            let device = AVCaptureDevice.default(for: AVMediaType.video),
+//            device.hasTorch
+//        else { return }
+//
+//        do {
+//            try device.lockForConfiguration()
+//            device.torchMode = on ? .on : .off
+//            device.unlockForConfiguration()
+//        } catch {
+//            print("Torch could not be used")
+//        }
+//    }
+    func setUpConstraints(){
+//        flash.snp.makeConstraints{make in
+//            make.centerX.equalToSuperview()
+//            make.bottom.equalToSuperview().inset(100)
+//            make.width.height.equalTo(60)
+//        }
+        trackButton.snp.makeConstraints{make in
+            make.centerX.centerY.equalToSuperview()
+            make.left.right.equalToSuperview().inset(40)
+        }
     }
 }
--- ImageTracker/ViewController.swift
+++ ImageTracker/ViewController.swift
@@ -8,14 +8,53 @@
 import UIKit
 import SceneKit
 import ARKit
+import UIKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet var sceneView: ARSCNView!
+//    @IBOutlet var sceneView: ARSCNView!
+    var sceneView: ARSCNView!
+    var counter = 0
+    private lazy var exitButton : UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "xmark"), for: .normal)
+        btn.tintColor = .white
+        btn.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
+        return btn
+    }()
+    private lazy var flash : UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "flashlight.on.fill"), for: .normal)
+        btn.tintColor = .white
+        btn.layer.borderWidth = 3
+        btn.layer.borderColor = UIColor.white.cgColor
+        btn.addTarget(self, action: #selector(tappedFlash), for: .touchUpInside)
+        btn.layer.cornerRadius = 30
+        return btn
+    }()
+    private lazy var playAV : UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "play"), for: .normal)
+        btn.tintColor = .white
+        btn.layer.cornerRadius = 30
+        btn.layer.borderWidth = 3
+        btn.layer.borderColor = UIColor.white.cgColor
+        return btn
+    }()
+    //load your scene
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        sceneView = ARSCNView()
         
+        //add it to parents subview
+        view.addSubview(sceneView)
+        sceneView.addSubview(exitButton)
+        sceneView.addSubview(flash)
+        sceneView.addSubview(playAV)
+        //add autolayout contstraints
+//        view.addSubview(sceneView)
+        setUpConstraints()
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -71,14 +110,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Detected plane
         let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width,
                              height: imageAnchor.referenceImage.physicalSize.height)
-     
+        
         plane.firstMaterial?.diffuse.contents = self.videoPlayer
      
         let planeNode = SCNNode(geometry: plane)
         planeNode.eulerAngles.x = -.pi / 2
      
         node.addChildNode(planeNode)
-     
+        
         self.videoPlayer.play()
         
         if let pointOfView = sceneView.pointOfView {
@@ -113,7 +152,51 @@ class ViewController: UIViewController, ARSCNViewDelegate {
      
         return AVPlayer(url: url)
     }()
-    
+    @objc func dismissView(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    @objc func tappedFlash(){
+        if (counter % 2 == 0){
+            toggleTorch(on: true)
+        }
+        else{
+            toggleTorch(on: false)
+        }
+        counter += 1
+    }
+    func toggleTorch(on: Bool) {
+        guard
+            let device = AVCaptureDevice.default(for: AVMediaType.video),
+            device.hasTorch
+        else { return }
+
+        do {
+            try device.lockForConfiguration()
+            device.torchMode = on ? .on : .off
+            device.unlockForConfiguration()
+        } catch {
+            print("Torch could not be used")
+        }
+    } 
+    func setUpConstraints(){
+        sceneView.snp.makeConstraints{make in
+            make.top.left.right.bottom.equalToSuperview()
+        }
+        exitButton.snp.makeConstraints{make in
+            make.top.equalToSuperview().inset(60)
+            make.left.equalToSuperview().inset(50)
+        }
+        flash.snp.makeConstraints{make in
+            make.right.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(60)
+            make.width.height.equalTo(60)
+        }
+        playAV.snp.makeConstraints{make in
+            make.width.height.equalTo(60)
+            make.right.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(150)
+        }
+    }
     
     
 }
